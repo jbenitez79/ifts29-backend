490bcc15f7f3e455ba00fa84595a81daa69fb1c1
ecviv <ecvivar@gmail.com>
2026-05-21 20:40:25 -0300

    Migración del módulo cuentas corrientes a MongoDB con Mongoose

--- routes/cuentaCorrienteRoutes.js
+++ routes/cuentaCorrienteRoutes.js
@@ -11,21 +11,30 @@ const {
     obtenerCuentasVista,
     obtenerDetalleCuentaVista,
     crearCuentaVista,
+    crearCuentaVistaPost,
     editarCuentaVista,
+    registrarCargaVistaPost,
+    registrarPagoVistaPost,
     eliminarCuentaVista,
+    eliminarCuentaVistaPost,
 } = require("../controllers/cuentaCorrienteController");
 
 router.get("/vista", obtenerCuentasVista);
 router.get("/vista/nuevo", crearCuentaVista);
-router.get("/vista/editar/:idCliente", editarCuentaVista);
-router.get("/vista/detalle/:idCliente", obtenerDetalleCuentaVista);
-router.get("/vista/eliminar/:idCliente", eliminarCuentaVista);
+router.get("/vista/detalle/:id", obtenerDetalleCuentaVista);
+router.get("/vista/editar/:id", editarCuentaVista);
+router.get("/vista/eliminar/:id", eliminarCuentaVista);
+
+router.post("/vista/nuevo", crearCuentaVistaPost);
+router.post("/vista/cargo", registrarCargaVistaPost);
+router.post("/vista/pago", registrarPagoVistaPost);
+router.post("/vista/eliminar/:id", eliminarCuentaVistaPost);
 
 router.get("/", obtenerCuentas);
 router.get("/cliente/:idCliente", obtenerCuentaPorClienteId);
 router.post("/", crearCuenta);
 router.post("/pago", registrarPago);
 router.post("/cargo", registrarCarga);
-router.post("/eliminar/:idCliente", eliminarCuenta);
+router.post("/eliminar/:id", eliminarCuenta);
 
 module.exports = router;
